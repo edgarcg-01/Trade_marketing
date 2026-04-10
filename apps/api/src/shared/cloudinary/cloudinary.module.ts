e1b533be31594b24a566ae302030d68cead28296
Edgar <corresbarrada@gmail.com>
2026-04-10 14:38:48 -0600

    feat: implement Cloudinary module for image storage integration

--- apps/api/src/shared/cloudinary/cloudinary.module.ts
+++ apps/api/src/shared/cloudinary/cloudinary.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { v2 as cloudinary } from 'cloudinary';
 import { CloudinaryService } from './cloudinary.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [ConfigModule],
   providers: [
     {
       provide: 'CLOUDINARY',
+      inject: [ConfigService],
       useFactory: () => {
         return cloudinary.config({
           cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
