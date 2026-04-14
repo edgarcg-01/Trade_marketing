de971f2976519ad2e13f500c0e665ba4971f384d
Edgar <corresbarrada@gmail.com>
2026-04-14 12:24:36 -0600

    fix: add migration to restore and reassign supervisor_id for production users

--- database/migrations/20260414130000_fix_supervisor_id_production.js
+++ database/migrations/20260414130000_fix_supervisor_id_production.js
@@ -5,32 +5,32 @@
 exports.up = async function(knex) {
   console.log('[fix_supervisor_id_production] Restaurando supervisor_id en producción...');
   
-  // Mapeo de supervisores basado en el seed original 01_users.js
+  // Mapeo de supervisores basado en el seed original 01_users.js (UUIDs completos)
   const supervisorMapping = {
-    'LA PIEDAD': '7dc8ef21', // angel_vazquez
-    'ZAMORA': 'f5ca24b4',    // francisco_martinez
-    'MORELIA': '504b1d53'   // jose_herrera
+    'LA PIEDAD': '7dc8ef21-8e65-4ffd-a5db-5a31a82b9b37', // angel_vazquez
+    'ZAMORA': 'f5ca24b4-4c08-473e-8991-c8a5377a26ed',    // francisco_martinez
+    'MORELIA': '504b1d53-a6ee-4eaf-a0ef-15b4ae9a2deb'   // jose_herrera
   };
   
   // Mapeo específico de usuario -> supervisor (basado en datos de producción proporcionados)
   const userToSupervisorMapping = {
-    'maria_rocha': '7dc8ef21',
-    'maria_valadez': '7dc8ef21',
-    'victor_mata': '7dc8ef21',
-    'victor_garcia': '7dc8ef21',
-    'mariano_martinez': '7dc8ef21',
-    'victorino_urbano': '7dc8ef21',
-    'joaquin_hurtado': '7dc8ef21',
-    'jose_garcia': '7dc8ef21',
-    'victor_zalapa': 'f5ca24b4',
-    'daniel_rojano': 'f5ca24b4',
-    'jose_munoz': 'f5ca24b4',
-    'jose_zavala': 'f5ca24b4',
-    'eduardo_miranda': '504b1d53',
-    'joseph_guerrero': '504b1d53',
-    'enrique_herrera': '504b1d53',
-    'guillermo_hernandez': '504b1d53',
-    'cesar_plascencia': '504b1d53'
+    'maria_rocha': '7dc8ef21-8e65-4ffd-a5db-5a31a82b9b37',
+    'maria_valadez': '7dc8ef21-8e65-4ffd-a5db-5a31a82b9b37',
+    'victor_mata': '7dc8ef21-8e65-4ffd-a5db-5a31a82b9b37',
+    'victor_garcia': '7dc8ef21-8e65-4ffd-a5db-5a31a82b9b37',
+    'mariano_martinez': '7dc8ef21-8e65-4ffd-a5db-5a31a82b9b37',
+    'victorino_urbano': '7dc8ef21-8e65-4ffd-a5db-5a31a82b9b37',
+    'joaquin_hurtado': '7dc8ef21-8e65-4ffd-a5db-5a31a82b9b37',
+    'jose_garcia': '7dc8ef21-8e65-4ffd-a5db-5a31a82b9b37',
+    'victor_zalapa': 'f5ca24b4-4c08-473e-8991-c8a5377a26ed',
+    'daniel_rojano': 'f5ca24b4-4c08-473e-8991-c8a5377a26ed',
+    'jose_munoz': 'f5ca24b4-4c08-473e-8991-c8a5377a26ed',
+    'jose_zavala': 'f5ca24b4-4c08-473e-8991-c8a5377a26ed',
+    'eduardo_miranda': '504b1d53-a6ee-4eaf-a0ef-15b4ae9a2deb',
+    'joseph_guerrero': '504b1d53-a6ee-4eaf-a0ef-15b4ae9a2deb',
+    'enrique_herrera': '504b1d53-a6ee-4eaf-a0ef-15b4ae9a2deb',
+    'guillermo_hernandez': '504b1d53-a6ee-4eaf-a0ef-15b4ae9a2deb',
+    'cesar_plascencia': '504b1d53-a6ee-4eaf-a0ef-15b4ae9a2deb'
   };
   
   // Paso 1: Identificar todos los usuarios colaboradores (incluso si ya tienen supervisor_id)
