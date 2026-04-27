0a1b256222e1357af74f63945e0d5e4668259212
Edgar <corresbarrada@gmail.com>
2026-04-27 14:58:28 -0600

    Fix import from @shared-models to @shared-auth/core in auth.service

--- apps/view/src/app/core/services/auth.service.ts
+++ apps/view/src/app/core/services/auth.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 import { tap } from 'rxjs/operators';
 import { Observable } from 'rxjs';
-import { JwtPayload, LoginResponse } from '@shared-models';
+import { JwtPayload, LoginResponse } from '@shared-auth/core';
 import { Permission } from '../constants/permissions';
 
 @Injectable({
